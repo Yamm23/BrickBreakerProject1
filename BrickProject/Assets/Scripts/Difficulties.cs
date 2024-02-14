ccb277e141fca7a09b2dcba0414b2079c7697114
Yamm23 <123715881+Yamm23@users.noreply.github.com>
2024-02-14 12:23:44 +0545

    14Feb First Commit

--- BrickProject/Assets/Scripts/Difficulties.cs
+++ BrickProject/Assets/Scripts/Difficulties.cs
@@ -10,4 +10,8 @@ public class LevelChooser : MonoBehaviour
     {
         SceneManager.LoadScene("World");
     }
+    public void EasyScene()
+    {
+        SceneManager.LoadScene("Easy");
+    }
 }
